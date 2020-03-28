2f21d280c4091995de7132b478f95f2bef08e48e
superchauzette <kevin.tillot@gmail.com>
2020-03-28 23:06:12 +0100

    conf webpack dynamic, when add a file in folder page build with new entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require("@symfony/webpack-encore");
+const fs = require("fs");
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -6,48 +7,24 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
 }
 
+Encore.setOutputPath("build/") // directory where compiled assets will be stored
+  .setPublicPath("/front/") // public path used by the web server to access the output path
+  .setManifestKeyPrefix("build/"); // only needed for CDN's or sub-directory deploy
+
+const filenames = fs.readdirSync("front/pages");
+filenames.forEach((filename) => {
+  Encore.addEntry(filename.replace(".tsx", ""), "./front/pages/" + filename);
+});
+
 Encore
-  // directory where compiled assets will be stored
-  .setOutputPath("build/")
-  // public path used by the web server to access the output path
-  .setPublicPath("/front/")
-  // only needed for CDN's or sub-directory deploy
-  .setManifestKeyPrefix("build/")
-
-  /*
-   * ENTRY CONFIG
-   *
-   * Add 1 entry for each "page" of your app
-   * (including one that's included on every page - e.g. "app")
-   *
-   * Each entry will result in one JavaScript file (e.g. app.js)
-   * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-   */
-  .addEntry("index", "./front/pages/index.tsx")
-  .addEntry("about", "./front/pages/about.tsx")
-  //.addEntry('page1', './assets/js/page1.js')
-  //.addEntry('page2', './assets/js/page2.js')
-
-  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-  // .splitEntryChunks()
-
   // will require an extra script tag for runtime.js
   // but, you probably want this, unless you're building a single-page app
   .enableSingleRuntimeChunk()
-
-  /*
-   * FEATURE CONFIG
-   *
-   * Enable & configure other features below. For a full
-   * list of features, see:
-   * https://symfony.com/doc/current/frontend.html#adding-more-features
-   */
+  // .splitEntryChunks() // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
   .cleanupOutputBeforeBuild()
   .enableBuildNotifications()
   .enableSourceMaps(!Encore.isProduction())
-  // enables hashed filenames (e.g. app.abc123.css)
-  //.enableVersioning(Encore.isProduction())
-
+  //.enableVersioning(Encore.isProduction())  // enables hashed filenames (e.g. app.abc123.css)
   .addLoader({
     test: /\.(js|jsx)$/,
     loader: "eslint-loader",
@@ -63,22 +40,13 @@ Encore
     config.useBuiltIns = "entry";
     config.corejs = 3;
   })
-
-  // enables Sass/SCSS support
-  //.enableSassLoader()
-
-  // uncomment if you use TypeScript
+  //.enableSassLoader()   // enables Sass/SCSS support
   .enableTypeScriptLoader()
-
-  // uncomment to get integrity="..." attributes on your script & link tags
-  // requires WebpackEncoreBundle 1.4 or higher
-  //.enableIntegrityHashes(Encore.isProduction())
-
-  // uncomment if you're having problems with a jQuery plugin
-  //.autoProvidejQuery()
-
-  // uncomment if you use API Platform Admin (composer require api-admin)
   .enableReactPreset();
-//.addEntry('admin', './assets/js/admin.js')
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
+//.autoProvidejQuery() // uncomment if you're having problems with a jQuery plugin
+//.addEntry('admin', './assets/js/admin.js') // uncomment if you use API Platform Admin (composer require api-admin)
 
 module.exports = Encore.getWebpackConfig();
